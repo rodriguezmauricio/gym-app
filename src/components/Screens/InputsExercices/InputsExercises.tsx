cbbd6a3bf18874e93de32bd69e677b044767aeca
rodriguezmauricio <contato.mauricioalves@gmail.com>
2023-09-06 21:11:44 -0300

    atualização design inputs

--- src/components/Screens/InputsExercices/InputsExercises.tsx
+++ src/components/Screens/InputsExercices/InputsExercises.tsx
@@ -1,19 +1,20 @@
 import { StyleSheet, Text, TextInput, View } from "react-native";
 import React from "react";
 import Checkbox from "../../Checkbox/Checkbox";
+import { themeColors } from "../../styles/CompStyles";
 
 const InputsExercises = () => {
   return (
-    <View>
+    <View style={styles.container}>
       <Text>Inputs Exercises</Text>
 
-      <TextInput placeholder="Nome do Exercício" />
+      <TextInput placeholder="Nome do Exercício" style={styles.inputs} />
 
-      <TextInput placeholder="Séries" />
+      <TextInput placeholder="Séries" style={styles.inputs} />
 
-      <TextInput placeholder="Reps" />
+      <TextInput placeholder="Reps" style={styles.inputs} />
 
-      <TextInput placeholder="Carga" />
+      <TextInput placeholder="Carga" style={styles.inputs} />
 
       <Checkbox content="Cada lado" />
     </View>
@@ -22,4 +23,14 @@ const InputsExercises = () => {
 
 export default InputsExercises;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    gap: 10,
+  },
+  inputs: {
+    borderColor: themeColors.grey,
+    borderWidth: 1,
+    padding: 10,
+    borderRadius: 5,
+  },
+});
